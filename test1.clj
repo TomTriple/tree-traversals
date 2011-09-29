4d3a0174f899fd2f8fd752224a02636adfc956da
thoefer <mail@tomhoefer.de>
2011-09-29 22:46:35 +0200

    Implemented basic test structure and env utils for handling command line args

--- test1.clj
+++ test1.clj
@@ -1,20 +1,55 @@
-(ns bin-tree
+
+(ns testing)
+
+  (defn- eval-assert
+    "Delegation target for all future assert functions"
+    [assertion]
+    (if assertion
+      (println "OK")
+      (throw (AssertionError.))))
+
+  (defn assert-equal
+    "Either a equals b or an assertion will be thrown"
+    [a b]
+    (eval-assert (= a b)))
+
+
+(ns env-utils)
+
+  (defn nth-arg-is 
+    "Returns true of the cmd argument at pos *pos* equals *expected*"
+    [pos expected]
+    (= (nth *command-line-args* pos) expected))
+    
+  (defn env-is 
+    "Returns true if the env argument equals *env*"
+    [env]
+    (nth-arg-is 0 env))
+  
+  (defmacro when-env-is 
+    "Only evaluates *env-action* when the env argument equals *env-type*. Each env has a short version e.g. test <=> t."
+    [env-type env-action]
+    (and (= env-type :test) (or (env-is "test") (env-is "t")) 
+      `(~env-action)))
+
+
+(ns tree-traversals
+  (:use 
+    (testing) (env-utils))
   (:import (java.util Random)))
 
-(defstruct node :id :value)
-
-(def nodes (ref ())) 
-
-(defn insert-node [node] 
-  (dosync 
-    ;(ref-set nodes (cons node @nodes))))
-    (alter nodes conj node)))
-
-(println @nodes)
-(insert-node (struct node 1 99))
-(insert-node (struct node 1 44))
-(println @nodes)
+  (defstruct node :id :value)
+  (def nodes (atom ()))
+  (defn insert-node [node]
+    (swap! nodes conj node))
 
 
+  (defn run-tests [] 
+    (do 
+      (insert-node (struct node 1 99))
+      (insert-node (struct node 1 44))
+      (testing/assert-equal (count @nodes) 2)
+      (println @nodes)))
 
+  (env-utils/when-env-is :test run-tests)
 
