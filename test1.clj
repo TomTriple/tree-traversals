f82966b96f5a98e25b5f940e8c50dd529567f9a2
thoefer <mail@tomhoefer.de>
2011-09-30 22:23:24 +0200

    traversal extended

--- test1.clj
+++ test1.clj
@@ -44,43 +44,44 @@
   (refer 'env-utils)
 
   (defstruct node-item :id :value :left :right)
-  (def root (atom nil))
+  (def root (ref nil))
   
   (defn- belongs-left? 
     "Returns true, if node-to-insert´s value key is greater than node´s value. "
     [node node-to-insert] 
     (> (node-to-insert :value) (node :value)))
     
-  (defn- belongs-right? 
+  (defn- belongs-right?
     "Is defined in terms of belongs-left? and returns the inverted boolean value. "
     [node node-to-insert]
     (not (belongs-left? node node-to-insert)))
 
   (defn insert-node
-    "Inserts a node to a binary, non-balanced search-tree with O(n) complexity."
+    "Inserts a node to a binary, non-balanced search-tree."
     [node-to-insert]
     (if @root
-      (loop [node @root]
-        (if (belongs-left? node node-to-insert)
-          (if (node :left)
-            (recur (node :left))
-;            (assoc node :left node-to-insert))
-            (println "AAA"))
-        (if (node :right) 
-            (recur (node :right)) 
-;            (swap! node assoc :right node-to-insert))))            
-;            (assoc node :right node-to-insert))))
-            (println "BBB"))))
-      (reset! root node-to-insert)))
+      (dosync 
+        (loop [node @root] 
+          (if (belongs-left? node node-to-insert) 
+            (if (node :left) 
+              (recur (node :left))
+              (assoc node :left node-to-insert))
+            (if (node :right) 
+              (recur (node :right)) 
+              (assoc node :right node-to-insert)))))
+      (dosync 
+        (ref-set root node-to-insert))))
 
 
 (defn run-tests []
     (assert-nil @root)
     (insert-node (struct node-item 1 99))
-    (insert-node (struct node-item 2 88)))
+    (insert-node (struct node-item 2 105))    
+    (insert-node (struct node-item 3 88)))
 
 
 (when-env-is :test run-tests)
+(println @root)
 
 
 
