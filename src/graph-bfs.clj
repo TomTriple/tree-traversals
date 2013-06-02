12ab5625f9c904499f41f8f19dd40d5edf5190f9
thoefer <mail@tomhoefer.de>
2013-06-02 18:56:56 +0200

    added graph traversal with bfs and shortest path with bfs tree

---
+++ src/graph-bfs.clj
@@ -0,0 +1,81 @@
+(defrecord Edge [weight target])
+(defrecord Node [label edges])
+
+
+(def graph (atom {}))
+
+
+(defn node-connect
+  "Connects the start node :src to node :dst with edge weight :weight. This fn supports arity overloading."
+  ([src dst weight]
+    (let [src-node (src @graph)
+          new-edges (conj (:edges src-node) (Edge. weight dst))]
+      (swap! graph assoc src
+        (assoc src-node :edges new-edges))))
+  ([src dst weight & args]
+    (doseq [x (partition 3 (apply list src dst weight args))]
+      (node-connect (nth x 0) (nth x 1) (nth x 2)))))
+
+
+(defn node-add
+  "Adds a node to the graph with an empty list of adjacent nodes."
+  [ & ids]
+  (swap! graph assoc reduce #(assoc %1 (Node. %2 [])) {} ids))
+
+
+(defn node-degree
+  "Returns the degree of a node."
+  [node]
+  (count (:edges node)))
+
+
+(defn graph-bfs
+  "Performs a breadth first search (bfs) on :graph starting at :p. Returns a bfs tree datastructure as map."
+  [graph start]
+  (loop [q [start]
+         states {start :discovered}
+         tree {start -1}]
+    (if-not (seq q)
+      tree
+      (do
+        ;(println "discovered: " (peek q))
+        (let [adj (vec (for [e (:edges ((peek q) graph)) :when (not (contains? states (:target e)))] (:target e)))]
+          (recur
+            (vec
+              (flatten (cons adj (pop q))))
+            (reduce #(assoc %1 %2 :discovered) states adj)
+            (reduce #(assoc %1 %2 (peek q)) tree adj)))))))
+
+
+(defn find-path
+  "Finds the shortests path from :start to :end."
+  [tree start end]
+  (loop [start start
+         path []]
+    (if (or (= start end) (end -1))
+      (conj path end)
+      (recur
+        (get tree start)
+        (conj path start)))))
+
+
+(node-add :grhh :bra :feilnbach :aibling :raubling :rosenheim :wasserburg)
+
+(node-connect
+  :grhh :raubling 5
+  :grhh :bra 2
+  :bra :grhh 2
+  :grhh :feilnbach 10
+  :feilnbach :aibling 5
+  :aibling :rosenheim 7
+  :raubling :rosenheim 10
+  :rosenheim :wasserburg 30
+)
+
+(let [graph-bfs-tree (graph-bfs @graph :bra)]
+  (println
+    (find-path graph-bfs-tree :wasserburg :bra)))
+
+
+
+
